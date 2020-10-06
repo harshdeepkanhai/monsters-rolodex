5841776c30a477cf3f2acc862fe49176921e423d
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2020-10-05 22:30:55 -0600

    Updates

---
+++ precache-manifest.1e9088620b4419845b39d527e7971da0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e9e394c5abedd27f11a03ba066e676",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d60f7270fb54c2ea3d05",
+    "url": "/monsters-rolodex/static/css/main.4b188630.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d60f7270fb54c2ea3d05",
+    "url": "/monsters-rolodex/static/js/main.fd9103f5.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
